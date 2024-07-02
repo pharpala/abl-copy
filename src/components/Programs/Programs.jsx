905a82b3d68e103065b02f34b05d1accf0ab34fd
Pranay Harpalani <pranayinspain@gmail.com>
2024-07-01 22:43:34 -0400

    home page done

--- src/components/Programs/Programs.jsx
+++ src/components/Programs/Programs.jsx
@@ -3,35 +3,41 @@ import './Programs.css'
 import trivia from '../../assets/trivia.png'
 import club from '../../assets/club.png'
 import freezie from '../../assets/freezie.png'
+import datefreezies from '../../assets/date-freezies.png'
+import datetrivia from '../../assets/date-trivia.png'
+import dateclub from '../../assets/date-club.png'
 
 const Programs = () => {
     return (
         <div className="programs-container">
-            <div>
-                <h1 className='header'>Upcoming Events</h1>
-                <h3>Check out our upcoming events!</h3>
-            </div>
+
             <div className="programs">
-              <div className='program'>
-                <img src={trivia} alt="event" />
-                <div className="caption">
-                    <p>Join us for trivia night at the bullring</p>
+                <div className='program'>
+                    <img src={trivia} alt="event" />
+                    <a href="https://gryphlife.uoguelph.ca/organization/cepssc/events" target="_blank" rel="noopener noreferrer">
+                        <div className="caption">
+                            <img src={datetrivia} alt="" />
+                        </div>
+                    </a>
                 </div>
-              </div>
 
-              <div className='program'>
-                <img src={club} alt="event" />
-                <div className="caption">
-                    <p>Join us for trivia night at the bullring</p>
+                <div className='program'>
+                    <img src={club} alt="event" />
+                    <a href="https://gryphlife.uoguelph.ca/organization/cepssc/events" target="_blank" rel="noopener noreferrer">
+                        <div className="caption">
+                            <img src={dateclub} alt="" />
+                        </div>
+                    </a>                    
                 </div>
-              </div>
 
-              <div className='program'>
-                <img src={freezie} alt="event" />
-                <div className="caption">
-                    <p>Join us for trivia night at the bullring</p>
+                <div className='program'>
+                    <img src={freezie} alt="event" />
+                    <a href="https://gryphlife.uoguelph.ca/organization/cepssc/events" target="_blank" rel="noopener noreferrer">
+                        <div className="caption">
+                            <img src={datefreezies} alt="" />
+                        </div>
+                    </a>
                 </div>
-              </div>
             </div>
         </div>
     )
