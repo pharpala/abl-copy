c71f78d392556ab9e3df7543d3840f9f21ccf1cc
Pranay Harpalani <pranayinspain@gmail.com>
2024-07-18 00:25:43 -0400

    fix media query for navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
@@ -63,6 +63,13 @@ const Documents = () => {
 
 
 const App = () => {
+
+  useEffect(() => {
+    // Log screen dimensions initially
+    console.log('Initial Screen Width:', window.innerWidth);
+    console.log('Initial Screen Height:', window.innerHeight);
+  }, []);
+
   return (
     <Router>
       <Navbar />
