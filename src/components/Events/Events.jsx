027cc8f0be570f644799e872ba9addfe433764ec
Pranay Harpalani <pranayinspain@gmail.com>
2024-11-19 12:12:19 -0500

    add new events

--- src/components/Events/Events.jsx
+++ src/components/Events/Events.jsx
@@ -6,6 +6,10 @@ import freezie from '../../assets/freezie.png'
 import datefreezies from '../../assets/date-freezies.png'
 import datetrivia from '../../assets/date-trivia.png'
 import dateclub from '../../assets/date-club.png'
+import linkedin from '../../assets/linkedin.png'
+import datelinkedin from '../../assets/datelinkedin.png'
+import mentalmath from '../../assets/mental-math.png'
+import datemath from '../../assets/datemath.png'
 
 const Events = () => {
     return (
@@ -13,30 +17,21 @@ const Events = () => {
 
             <div className="programs">
                 <div className='program'>
-                    <img src={trivia} alt="event" />
-                    <a href="https://gryphlife.uoguelph.ca/event/265304" target="_blank" rel="noopener noreferrer">
+                    <img src={linkedin} alt="event" />
+                    <a href="https://experienceguelph.ca/myAccount/career/events-workshops.htm" target="_blank" rel="noopener noreferrer">
                         <div className="caption">
-                            <img src={datetrivia} alt="" />
+                            <img src={datelinkedin} alt="" />
                         </div>
                     </a>
                 </div>
 
                 <div className='program'>
-                    <img src={club} alt="event" />
+                    <img src={mentalmath} alt="event" />
                     <a href="https://gryphlife.uoguelph.ca/event/265270" target="_blank" rel="noopener noreferrer">
                         <div className="caption">
-                            <img src={dateclub} alt="" />
+                            <img src={datemath} alt="" />
                         </div>
                     </a>                    
-                </div>
-
-                <div className='program'>
-                    <img src={freezie} alt="event" />
-                    <a href="https://gryphlife.uoguelph.ca/event/265248" target="_blank" rel="noopener noreferrer">
-                        <div className="caption">
-                            <img src={datefreezies} alt="" />
-                        </div>
-                    </a>
                 </div>
             </div>
         </div>
